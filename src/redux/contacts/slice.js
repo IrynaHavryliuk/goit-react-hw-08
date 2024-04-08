95e3a61e6965b90f711891dcbc7ed4b1b977d36c
Iryna Havryliuk <gis.80@ukr.net>
2024-04-08 13:23:47 -0400

    add

--- src/redux/contacts/slice.js
+++ src/redux/contacts/slice.js
@@ -1,6 +1,6 @@
 // slice.js
 import { createSlice } from "@reduxjs/toolkit";
-import { createSelector } from "@reduxjs/toolkit";
+import { createSelector } from "reselect"; // Змінено імпорт createSelector
 import { fetchContacts, addContact, deleteContact } from "./operations.js";
 import { logOut } from "../auth/operations.js";
 import { selectNameFilter } from "../filters/selectors.js";
